c0bede9b9f334922899ae9e1ee8dbdc4d400bf83
Justin Crapse <justincrapse@gmail.com>
2022-06-09 19:41:34 -0700

    get speedtree updated

--- python3.7libs/ox/nodes/redshift_nodes/sprite_node.py
+++ python3.7libs/ox/nodes/redshift_nodes/sprite_node.py
@@ -6,7 +6,7 @@ from ox.base_objects.menu import Menu
 
 class SpriteNode(OXNode):
     node_type = 'redshift::Sprite'
-    parm_lookup_dict = {'shader_input_0': 'Shader_Input_0', 'inputr': 'inputr', 'inputg': 'inputg', 'inputb': 'inputb', 'inputa': 'inputa', 'stencil_1': 'Stencil_1', 'tex0': 'tex0', 'tspace_id': 'tspace_id', 'mode': 'mode', 'threshold': 'threshold', 'repeats1': 'repeats1', 'repeats2': 'repeats2', 'tex0_colorspace': 'tex0_colorSpace', 'tex0_gamma': 'tex0_gamma'}
+    parm_lookup_dict = {'shader_input_0': 'Shader_Input_0', 'inputr': 'inputr', 'inputg': 'inputg', 'inputb': 'inputb', 'inputa': 'inputa', 'stencil_1': 'Stencil_1', 'tex0': 'tex0', 'tspace_id': 'tspace_id', 'mode': 'mode', 'threshold': 'threshold', 'repeats1': 'repeats1', 'repeats2': 'repeats2', 'tex0_colorspace': 'tex0_colorSpace', 'tex0_gamma': 'tex0_gamma', 'shader_skipdefvalparms': 'shader_skipdefvalparms'}
 
     def __init__(self, node=None, ox_parent=None, node_name=None):
         self.ox_parent = ox_parent
@@ -24,15 +24,16 @@ class SpriteNode(OXNode):
         self.parm_inputb = Parameter(parm=self.node.parm('inputb'))
         self.parm_inputa = Parameter(parm=self.node.parm('inputa'))
         self.parm_stencil_1 = Parameter(parm=self.node.parm('Stencil_1'))
+        self.parm_tex0 = Parameter(parm=self.node.parm('tex0'))
         self.parm_tspace_id = Parameter(parm=self.node.parm('tspace_id'))
         self.parm_threshold = Parameter(parm=self.node.parm('threshold'))
         self.parm_repeats1 = Parameter(parm=self.node.parm('repeats1'))
         self.parm_repeats2 = Parameter(parm=self.node.parm('repeats2'))
         self.parm_tex0_gamma = Parameter(parm=self.node.parm('tex0_gamma'))
+        self.parm_shader_skipdefvalparms = Parameter(parm=self.node.parm('shader_skipdefvalparms'))
 
         
         # parm menu vars:
-        self.parm_tex0 = Tex0Menu(parm=self.node.parm('tex0'))
         self.parm_mode = ModeMenu(parm=self.node.parm('mode'))
         self.parm_tex0_colorspace = Tex0ColorspaceMenu(parm=self.node.parm('tex0_colorSpace'))
 
@@ -42,52 +43,34 @@ class SpriteNode(OXNode):
 
 
 # parm menu classes:
-class Tex0Menu(Menu):
-    def __init__(self, parm):
-        self.parm = parm
-        super().__init__(parm=parm)
-        self.menu_e__art_projects____anga_veld_4k_exr = 0
-        self.menu_e__art_3d_00_my____ring_opacity_png = 1
-        self.menu_e__art_projects____ly_cloudy_4k_exr = 2
-        self.menu_e__art_projects_____1d_clear_4k_exr = 3
-        self.menu_op__opfullpath______gen_displace___ = 4
-        self.menu_op__opfullpath__________ocean_s____ = 5
-        self.menu_op__opfullpath_____ean_interior____ = 6
-        self.menu_e__art_3d_eucaly___ark_3_height_png = 7
-        self.menu_e__art_projects____quarry_02_4k_exr = 8
-        self.menu_e__art_projects____a_sunrise_4k_exr = 9
-        self.menu_e__art_projects____in_cellar_4k_exr = 10
-        self.menu_e__art_projects____oon_grass_4k_exr = 11
-
-
 class ModeMenu(Menu):
     def __init__(self, parm):
         self.parm = parm
         super().__init__(parm=parm)
-        self.menu_from_color_intensity = 0
-        self.menu_from_alpha = 1
+        self.menu_from_color_intensity = '0'
+        self.menu_from_alpha = '1'
 
 
 class Tex0ColorspaceMenu(Menu):
     def __init__(self, parm):
         self.parm = parm
         super().__init__(parm=parm)
-        self.menu_auto = 0
-        self.menu_srgb = 1
-        self.menu_adobergb = 2
-        self.menu_acescg = 3
-        self.menu_aces2065_1 = 4
-        self.menu_scene_linear_rec_709_srgb = 5
-        self.menu_scene_linear_dci_p3_d65 = 6
-        self.menu_scene_linear_rec_2020 = 7
-        self.menu_raw = 8
-        self.menu_acescct = 9
-        self.menu_arri_logc___alexawidegamut = 10
-        self.menu_red_log3g10___redwidegamutrgb = 11
-        self.menu_sony_slog3___sgamut3 = 12
-        self.menu_panasonic_v_log___v_gamut = 13
-        self.menu_log_film_scan__adx10_ = 14
-        self.menu_invert_aces_1_0_sdr_video = 15
+        self.menu_auto = ''
+        self.menu_srgb = 'sRGB'
+        self.menu_adobergb = 'AdobeRGB'
+        self.menu_acescg = 'ACEScg'
+        self.menu_aces2065_1 = 'ACES2065-1'
+        self.menu_scene_linear_rec_709_srgb = 'scene-linear Rec.709-sRGB'
+        self.menu_scene_linear_dci_p3_d65 = 'scene-linear DCI-P3 D65'
+        self.menu_scene_linear_rec_2020 = 'scene-linear Rec.2020'
+        self.menu_raw = 'Raw'
+        self.menu_acescct = 'ACEScct'
+        self.menu_arri_logc___alexawidegamut = 'ARRI LogC / AlexaWideGamut'
+        self.menu_red_log3g10___redwidegamutrgb = 'RED Log3G10 / REDWideGamutRGB'
+        self.menu_sony_slog3___sgamut3 = 'Sony SLog3 / SGamut3'
+        self.menu_panasonic_v_log___v_gamut = 'Panasonic V-Log / V-Gamut'
+        self.menu_log_film_scan__adx10_ = 'Log film scan (ADX10)'
+        self.menu_invert_aces_1_0_sdr_video = 'Invert ACES 1.0 SDR-video'
 
 
 
--- python3.7libs/ox/utils/gen_classes/gen_menu_classes.py
+++ python3.7libs/ox/utils/gen_classes/gen_menu_classes.py
@@ -1,14 +1,15 @@
 from ox.utils import data_lover
+import hou
 
 
-def gen_menu_class_as_string(parm):
+def gen_menu_class_as_string(parm: hou.Parm):
     parm_name = parm.name()
     parm_class_name = data_lover.get_str_as_class(parm_name)
     menu_list = parm.menuLabels()
     menu_var_list = [data_lover.get_str_as_py_var(i) for i in menu_list]
     menu_item_list = [i for i in parm.menuItems()]
-    # menu_list_lines = ''.join([f"\t\tself.{item} = '{value}'\n" for item, value in zip(menu_var_list, menu_item_list)])
-    menu_list_lines = ''.join([f"\t\tself.menu_{var} = {index}\n" for index, var in enumerate(menu_var_list)])
+    menu_list_lines = ''.join([f"\t\tself.menu_{item} = '{value}'\n" for item, value in zip(menu_var_list, menu_item_list)])
+    # menu_list_lines = ''.join([f"\t\tself.menu_{var} = {index}\n" for index, var in enumerate(menu_var_list)])
     class_string = f"""class {parm_class_name}Menu(Menu):
     def __init__(self, parm):
         self.parm = parm
